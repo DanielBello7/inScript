2f3cc4a9e4557411faeeaf47c39aa7d886132dc0
Daniel Bello <Danielbello@DanielBello.local>
2022-06-05 20:39:19 +0100

    li

--- src/middlewares/databaseConnection.ts
+++ src/middlewares/databaseConnection.ts
@@ -5,9 +5,12 @@
 import mongoose from 'mongoose';
 
 
+// connect function
 async function connect(databaseURL: string, callback?: Function) {
-  const databaseOptions: mongoose.ConnectOptions = {}
-  return await mongoose.connect(databaseURL, databaseOptions, () => callback && callback());
+     const databaseOptions: mongoose.ConnectOptions = {}
+     return await mongoose.connect(databaseURL, databaseOptions, () => callback && callback());
 }
 
+
+// main exports
 export default connect;
