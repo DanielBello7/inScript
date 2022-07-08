314328e4e25343eeac7e8e6f8b72185e7b823a2c
Daniel Bello <Danielbello@DanielBello.local>
2022-07-08 13:49:46 +0100

    ref

--- src/controllers/notifications.controller.ts
+++ src/controllers/notifications.controller.ts
@@ -8,87 +8,65 @@ import Log from "../config/bunyan.config";
 
 
 class Notifications {
-     public conn;
+   public conn;
 
-     constructor(connection: DatabaseType) {
-          this.conn = connection;
-     }
-
-
-     GetNotifications = async (req: RequestInterface, res: Response) => {
-
-          const email = req.user.email;
-
-          try {
-
-               const response = await this.conn.GetNotifications(email);
-
-               return res.json({payload: response});
+   constructor(connection: DatabaseType) {
+      this.conn = connection;
+   }
 
 
-          } catch (error: any) {
-               Log.error(error);
-               return res.status(500).json({msg: error.message});
-          }
-     }
+   GetNotifications = async (req: RequestInterface, res: Response) => {
+      const email = req.user.email;
+      try {
+         const response = await this.conn.GetNotifications(email);
+         return res.json({payload: response});
+      } 
+      catch (error: any) {
+         Log.error(error);
+         return res.status(500).json({msg: error.message});
+      }
+   }
 
-     SetNotificationAsRead = async (req: RequestInterface, res: Response) => {
+   SetNotificationAsRead = async (req: RequestInterface, res: Response) => {
+      const ID = req.params.notificationID;
+      const email = req.user.email;
+      try {
+         const response = await this.conn.ChangeNotificationStatus(email, ID);
+         if (!response) return res.status(400).json({msg: 'error'});
+         return res.json({msg: 'notification modified'});
+      } 
+      catch (error: any) {
+         Log.error(error);
+         return res.status(500).json({msg: error.message});
+      }
+   }
 
-          const ID = req.params.notificationID;
+   RemoveNotification = async (req: RequestInterface, res: Response) => {
+      const ID = req.params.notificationID;
+      const email = req.user.email;
+      try {
+         const response = await this.conn.DeleteNotification(email, ID);
+         if (!response) return res.status(400).json({msg: 'error'});
+         return res.json({msg: 'success'});
+      } 
+      catch (error: any) {
+         Log.error(error);
+         return res.status(500).json({msg: error.message});
+      }
+   }
 
-          const email = req.user.email;
-
-          try {
-
-               const response = await this.conn.ChangeNotificationStatus(email, ID);
-
-               if (!response) return res.status(400).json({msg: 'error'});
-
-               return res.json({msg: 'notification modified'});
-
-          } catch (error: any) {
-               Log.error(error);
-               return res.status(500).json({msg: error.message});
-          }
-     }
-
-     RemoveNotification = async (req: RequestInterface, res: Response) => {
-
-          const ID = req.params.notificationID;
-
-          const email = req.user.email;
-
-          try {
-
-               const response = await this.conn.DeleteNotification(email, ID);
-
-               if (!response) return res.status(400).json({msg: 'error'});
-
-               return res.json({msg: 'success'});
-
-          } catch (error: any) {
-               Log.error(error);
-               return res.status(500).json({msg: error.message});
-          }
-     }
-
-     ClearNotifications = async (req: RequestInterface, res: Response) => {
-
-          const email = req.user.email;
-
-          try {
-
-               const response = await this.conn.ClearAllNotifications(email);
-
-               if (!response) return res.status(400).json({msg: 'error'});
-
-               return res.json({msg: 'success'});
-
-          } catch (error: any) {
-               Log.error(error);
-               return res.status(500).json({msg: error.message});
-          }
-     }
+   ClearNotifications = async (req: RequestInterface, res: Response) => {
+      const email = req.user.email;
+      try {
+         const response = await this.conn.ClearAllNotifications(email);
+         if (!response) return res.status(400).json({msg: 'error'});
+         return res.json({msg: 'success'});
+      } 
+      catch (error: any) {
+         Log.error(error);
+         return res.status(500).json({msg: error.message});
+      }
+   }
 }
 
 export default Notifications;
