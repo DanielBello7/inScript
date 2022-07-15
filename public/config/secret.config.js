e8894c88429b2e99556a101667423df7cc4cfb89
Daniel Bello <gokebello@gmail.com>
2022-07-15 03:03:26 +0100

    removal

--- public/config/secret.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// main secret function
-function ExpressSecret(req, res, next) {
-    req.secret = process.env.SECRET;
-    return next();
-}
-exports.default = ExpressSecret;
